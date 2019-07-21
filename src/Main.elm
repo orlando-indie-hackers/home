032d0ec192c0fac19fd8a52f2c34bf87221db150
Bijan <bijanbwb@gmail.com>
2019-07-21 09:51:48 -0400

    Working on responsive design

--- src/Main.elm
+++ src/Main.elm
@@ -15,10 +15,29 @@ import Svg.Attributes
 
 main : Html a
 main =
-    div [ Html.Attributes.class "flex" ]
-        [ content
-        , sidebar
+    div [ Html.Attributes.class "flex flex-row-reverse flex-wrap" ]
+        [ splash
+        , content
         ]
+
+
+
+-- SPLASH IMAGE
+
+
+splash : Html a
+splash =
+    div [ Html.Attributes.class "bg-indigo-900 h-64 sm:h-screen sm:w-1/2 w-full" ]
+        [ image ]
+
+
+image : Html a
+image =
+    img
+        [ Html.Attributes.class "h-screen object-cover"
+        , Html.Attributes.src "https://createwithelm.s3.amazonaws.com/indie-hackers/wall-of-products.jpg"
+        ]
+        []
 
 
 
@@ -27,7 +46,7 @@ main =
 
 content : Html a
 content =
-    div [ Html.Attributes.class "bg-white h-screen w-1/2" ]
+    div [ Html.Attributes.class "bg-white sm:h-screen sm:w-1/2 w-full" ]
         [ div [ Html.Attributes.class "px-16 pt-16" ]
             [ header
             , subheader
@@ -57,6 +76,8 @@ subheader =
             , a
                 [ Html.Attributes.class "border-solid border-b-4 border-red-500 hover:border-red-300"
                 , Html.Attributes.href "https://indiehackers.com"
+                , Html.Attributes.target "_blank"
+                , Html.Attributes.title "Go to indiehackers.com"
                 ]
                 [ text "Indie Hackers" ]
             , span [] [ text " community for aspiring entrepreneurs and early founders of profitable businesses." ]
@@ -80,42 +101,62 @@ next =
 
 rsvpButton : Html a
 rsvpButton =
-    div [ Html.Attributes.class "mt-12" ]
+    div [ Html.Attributes.class "mt-10" ]
         [ a
-            [ Html.Attributes.class "bg-indigo-700 font-semibold hover:bg-indigo-900 pl-8 pr-10 py-4 rounded-full text-xl text-white uppercase"
+            [ Html.Attributes.class "bg-indigo-700 flex flex-no-wrap font-semibold hover:bg-indigo-900 items-center justify-center max-w-sm px-10 py-4 rounded-full text-xl text-white uppercase"
             , Html.Attributes.href "https://www.meetup.com/orlando-indie-hackers/events"
             ]
-            [ span [ Html.Attributes.class "text-red-500" ]
+            [ span [ Html.Attributes.class "mr-1 text-red-500" ]
                 [ Icon.meetup ]
-            , span [] [ text "RSVP on Meetup" ]
+            , span []
+                [ text "RSVP on Meetup" ]
             ]
         ]
 
 
 newsletterButton : Html a
 newsletterButton =
-    div [ Html.Attributes.class "mt-12" ]
+    div [ Html.Attributes.class "mt-6" ]
         [ a
-            [ Html.Attributes.class "bg-gray-600 font-semibold hover:bg-indigo-700 pl-8 pr-8 py-4 rounded-full text-xl text-white uppercase"
+            [ Html.Attributes.class "bg-gray-600 flex flex-no-wrap font-semibold hover:bg-indigo-700 items-center justify-center max-w-sm px-10 py-4 rounded-full text-xl text-white uppercase"
             , Html.Attributes.href "https://convertkit.com"
             ]
-            [ span [ Html.Attributes.class "text-white" ]
+            [ span [ Html.Attributes.class "mr-1 text-white" ]
                 [ Icon.email ]
-            , span [] [ text "Newsletter Sign-up" ]
+            , span []
+                [ text "Newsletter Sign-up" ]
             ]
         ]
+
+
+
+-- CONTACT
 
 
 contact : Html a
 contact =
-    div [ Html.Attributes.class "mt-8" ]
-        [ a [ Html.Attributes.href "https://twitter.com/bijanbwb" ]
-            [ Icon.twitter ]
-        , a [ Html.Attributes.href "mailto:bijanbwb@gmail.com" ]
+    div [ Html.Attributes.class "mt-10" ]
+        [ a
+            [ Html.Attributes.class "mr-2 hover:text-blue-300"
+            , Html.Attributes.href "https://twitter.com/bijanbwb"
+            ]
+            [ Icon.twitter
+            ]
+        , a
+            [ Html.Attributes.class "mr-2 hover:text-pink-300"
+            , Html.Attributes.href "mailto:bijanbwb@gmail.com"
+            ]
             [ Icon.email ]
-        , a [ Html.Attributes.href "https://github.com/orlando-indie-hackers" ]
+        , a
+            [ Html.Attributes.class "mr-2 hover:text-gray-300"
+            , Html.Attributes.href "https://github.com/orlando-indie-hackers"
+            ]
             [ Icon.github ]
         ]
+
+
+
+-- FUN
 
 
 emoji : Html a
@@ -124,22 +165,3 @@ emoji =
         [ span [ Html.Attributes.class "text-6xl" ]
             [ text "🚀" ]
         ]
-
-
-
--- SIDEBAR
-
-
-sidebar : Html a
-sidebar =
-    div [ Html.Attributes.class "bg-indigo-900 h-screen w-1/2" ]
-        [ image ]
-
-
-image : Html a
-image =
-    img
-        [ Html.Attributes.class "h-screen object-cover"
-        , Html.Attributes.src "https://createwithelm.s3.amazonaws.com/indie-hackers/wall-of-products.jpg"
-        ]
-        []
